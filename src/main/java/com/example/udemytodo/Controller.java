feb26a6376346b650c0f7793bf0f78239a2294c9
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-25 14:03:50 -0400

    Initial Refactors To Template

---
+++ src/main/java/com/example/udemytodo/Controller.java
@@ -0,0 +1,5 @@
+package com.example.udemytodo;
+
+public class Controller {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.example.udemytodo {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                    requires org.kordamp.ikonli.javafx;
-                
+
+    requires org.kordamp.ikonli.javafx;
+
     opens com.example.udemytodo to javafx.fxml;
     exports com.example.udemytodo;
 }
